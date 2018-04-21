8f0ae1de43dbbb81f13be61d3431963838d1aff8
xrstf <git@xrstf.de>
2018-04-21 18:47:20 +0200

    use PCRE instead of Go's regex engine, because Go is pretty slow at parsing simple logfile lines

--- Makefile
+++ Makefile
@@ -15,4 +15,5 @@ static:
 
 deps:
 	go get gopkg.in/yaml.v2
+	go get github.com/gijsbers/go-pcre
 	go get golang.org/x/tools/cmd/goimports
--- parser/nginx.go
+++ parser/nginx.go
@@ -3,17 +3,17 @@ package parser
 import (
 	"errors"
 	"fmt"
-	"regexp"
 	"strconv"
 	"time"
 
+	pcre "github.com/gijsbers/go-pcre"
 	"github.com/xrstf/logstats"
 )
 
 var nginxInvalidLine = errors.New("Could not parse log line")
 
 type nginxParser struct {
-	lineRegex *regexp.Regexp
+	lineRegex pcre.Regexp
 }
 
 func NewNginxParser() *nginxParser {
@@ -29,37 +29,37 @@ func NewNginxParser() *nginxParser {
 	regex := fmt.Sprintf(`^(%s) - %s \[(%s)\] "(%s) (%s) (%s)" (%s) (%s)`, ipRegex, userRegex, timeRegex, methodRegex, uriRegex, protocolRegex, statusRegex, sizeRegex)
 
 	return &nginxParser{
-		lineRegex: regexp.MustCompile(regex),
+		lineRegex: pcre.MustCompile(regex, 0),
 	}
 }
 
 func (s *nginxParser) ParseLine(line string) (*logstats.LogLine, error) {
-	match := s.lineRegex.FindStringSubmatch(line)
-	if match == nil {
+	matcher := s.lineRegex.MatcherString(line, 0)
+	if !matcher.Matches() {
 		return nil, nginxInvalidLine
 	}
 
-	parsed, err := time.Parse("02/Jan/2006:15:04:05 -0700", match[2])
+	parsed, err := time.Parse("02/Jan/2006:15:04:05 -0700", matcher.GroupString(2))
 	if err != nil {
 		return nil, nginxInvalidLine
 	}
 
-	statusCode, err := strconv.ParseInt(match[6], 10, 16)
+	statusCode, err := strconv.ParseInt(matcher.GroupString(6), 10, 16)
 	if err != nil {
 		return nil, nginxInvalidLine
 	}
 
-	size, err := strconv.ParseInt(match[7], 10, 64)
+	size, err := strconv.ParseInt(matcher.GroupString(7), 10, 64)
 	if err != nil {
 		return nil, nginxInvalidLine
 	}
 
 	return &logstats.LogLine{
-		IP:       match[1],
+		IP:       matcher.GroupString(1),
 		Date:     parsed,
-		Method:   match[3],
-		Uri:      match[4],
-		Protocol: match[5],
+		Method:   matcher.GroupString(3),
+		Uri:      matcher.GroupString(4),
+		Protocol: matcher.GroupString(5),
 		Status:   int(statusCode),
 		Size:     size,
 	}, nil
